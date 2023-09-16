c21064c41843cbc7a99a5bec5562f2f5e5239489
Jeedmark99 <jeedmark@icloud.com>
2023-09-16 23:11:43 +0200

    22 added use-state and some html

--- src/config/components/create.js
+++ src/config/components/create.js
@@ -3,7 +3,34 @@ import { useState } from 'react'
 import React from 'react'
 
 const create = () => {
+const[title, setTitle] = useState("");
+const[description, setDescription] = useState("");
 
+
+
+return (
+    <div>
+        <h1>Create a post</h1>
+        <form id='create-post-form'>
+         <label htmlFor='title'>TITLE:</label>
+         <textarea
+         name='title'
+         id='title'
+         cols={50}
+         rows={10}
+         required
+         ></textarea>
+         <label htmlFor='description'>DESCRIPTION:</label>
+         <textarea
+         name='description'
+         id='description'
+         cols={50}
+         rows={10}
+         ></textarea>   
+         <input type='submit' value="Create"></input>
+        </form>
+    </div>
+)
 }
 
 export default create;
