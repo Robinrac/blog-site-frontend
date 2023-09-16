cef5a2edc89bd216954b20a7912ed65117e7fb61
Jeedmark99 <jeedmark@icloud.com>
2023-09-16 23:19:10 +0200

    id-22 added event handlers

--- src/components/create.js
+++ src/components/create.js
@@ -1,4 +1,4 @@
-import '../App.css'
+
 import { useState } from 'react'
 import React from 'react'
 
@@ -6,16 +6,33 @@ const create = () => {
 const[title, setTitle] = useState("");
 const[description, setDescription] = useState("");
 
+const handleTitleChange = (e) => {
+    setTitle(e.target.value)
+}
+const handleDescriptionChange = (e) => {
+    setDescription(e.target.value)
+} 
+const handleSubmit = (e) => {
+    e.preventDefault()
 
+    const blogDataObject = {
+        title,
+        description
+    }
+    console.log(blogDataObject)
+    console.log(JSON.stringify(blogDataObject))
+}
 
 return (
     <div>
         <h1>Create a post</h1>
-        <form id='create-post-form'>
+        <form id='create-post-form' onSubmit={handleSubmit}>
          <label htmlFor='title'>TITLE:</label>
          <textarea
          name='title'
          id='title'
+         value={title}
+         onChange={handleTitleChange}
          cols={50}
          rows={10}
          required
@@ -23,6 +40,8 @@ return (
          <label htmlFor='description'>DESCRIPTION:</label>
          <textarea
          name='description'
+         value={description}
+         onChange={handleDescriptionChange}
          id='description'
          cols={50}
          rows={10}
