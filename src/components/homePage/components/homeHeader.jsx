ccb9b772c9d5663969ef91b345645d3ba1f65feb
Robinrac <robin.raczkiewicz@gmail.com>
2023-09-18 12:54:40 +0200

    (#21) Added styling files and routes

---
+++ src/components/homePage/components/homeHeader.jsx
@@ -0,0 +1,11 @@
+
+function HomeHeader() {
+
+    return(
+        <>
+            <h1>THIS IS THE HEADER OF THE HOME PAGE</h1>
+        </>
+    )
+}
+
+export default HomeHeader;
--- src/components/homePage/homePage.jsx
+++ src/components/homePage/homePage.jsx
@@ -1,8 +1,10 @@
 import BlogPosts from "./components/BlogPosts";
+import HomeHeader from "./components/homeHeader";
 
 const HomePage = () => {
   return (
     <div>
+      <HomeHeader />
       <BlogPosts />
     </div>
   );
