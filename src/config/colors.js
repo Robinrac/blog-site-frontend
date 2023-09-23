4160f5985711d36a108fd269134cc9413978128a
Robin Raczkiewicz <robin.raczkiewicz@gmail.com>
2023-09-24 00:46:07 +0200

    (#18) Final color change

--- src/config/colors.js
+++ src/config/colors.js
@@ -1,7 +1,7 @@
 const colors = {
     BACKGROUND: "#ebebeb", //white/grey background color
-    ACCENT_SECOND: "#5988ff", // blue color
-    ACCENT_THIRD: "#3862cf", //darker blue color
+    ACCENT_SECOND: "#0063A2", // blue color
+    ACCENT_THIRD: "#015588", //darker blue color
     HIGHLIGHT_ACCENT: "#cccf3e", //Yellow accent color
     PRIMARY_TEXT: "#fff",
 };
