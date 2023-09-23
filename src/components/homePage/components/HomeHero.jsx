434efc5a4da4ee7682305064d83aab3d7942f069
Robin Raczkiewicz <robin.raczkiewicz@gmail.com>
2023-09-24 01:24:57 +0200

    (#18, #61) added hero + blog design fix

--- src/components/homePage/components/HomeHero.jsx
+++ src/components/homePage/components/HomeHero.jsx
@@ -5,10 +5,7 @@ function HomeHero() {
     return(
             <div className="hero">
                 <div className="hero-content">
-                    <h1 className="hero-title">Welcome to My Blog</h1>
-                    <p className="hero-description">
-                    Explore the latest articles and insights on various topics.
-                    </p>
+                    <h1 className="hero-title">Welcome to Blogs.com</h1>
                 </div>
             </div>
     )
