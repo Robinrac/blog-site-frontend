8a4596b30de227bbd04dc9bcc9186da2613f4f72
Robin Raczkiewicz <robin.raczkiewicz@gmail.com>
2023-09-24 02:36:23 +0200

    (#18, #61, #60) hero + blog redesign + colors.js

--- src/config/colors.js
+++ src/config/colors.js
@@ -1,5 +1,6 @@
 const colors = {
     BACKGROUND: "#ebebeb", //white/grey background color
+    BACKGROUND_SECOND: "#7097debc", //Lightblue background
     ACCENT_SECOND: "#0063A2", // blue color
     ACCENT_THIRD: "#015588", //darker blue color
     HIGHLIGHT_ACCENT: "#cccf3e", //Yellow accent color
