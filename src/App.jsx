8105198833f30b5ebc8f9f0872966ad8ae114e7d
Jeedmark99 <jeedmark@icloud.com>
2023-09-29 01:31:13 +0200

    (#56)corrected the structure in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CreatePage from './components/createPage/CreatePage';
 import HomePage from './components/homePage/homePage';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
+function App() {
+ return(
+ <Router>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/create" element={<CreatePage />} />
     </Routes>
   </Router>
 );
+}
 
 export default App
