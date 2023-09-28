a5b967f149900aae5e1b6ca07bc91e991f4a91d5
Ehsan Hussain <ehsan-1995@hotmail.com>
2023-09-28 12:22:47 +0200

    #56 uppdatering

--- src/components/createPage/CreatePage.jsx
+++ src/components/createPage/CreatePage.jsx
@@ -1,9 +1,11 @@
+import BlogPosts from "../homePage/components/blogPosts";
 import CreatePost from "./components/CreatePost";
 
 const CreatePage = () => {
     return(
         <div>
             <CreatePost />
+            <BlogPosts />
 
         </div>
     )
