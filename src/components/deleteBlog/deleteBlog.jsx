12ba0b5412af224a41bf6ff5afa997099ec90743
Ehsan Hussain <ehsan-1995@hotmail.com>
2023-09-28 12:22:04 +0200

    #23 Uppdatering

--- src/components/deleteBlog/deleteBlog.jsx
+++ src/components/deleteBlog/deleteBlog.jsx
@@ -2,10 +2,11 @@ import PropTypes from "prop-types";
 
 const DeleteButton = ({ onDelete }) => {
   const handleDelete = () => {
+    onDelete ();
 
   };
 
-  return <button onClick={handleDelete}>Radera</button>;
+  return <button onClick={handleDelete}>Delete</button>;
 };
 
 DeleteButton.propTypes = {
